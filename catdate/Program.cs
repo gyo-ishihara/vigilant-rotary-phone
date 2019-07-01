48f00dde684c18fd305ed7520d1bab33f1b391e5
Gyo Ishihara <stf0404gy263@gmail.com>
2019-07-01 14:17:24 +0900

    Initial Commit
    My git commit.

---
+++ catdate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace catdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"現在の日時 :{DateTime.Now}");
+        }
+    }
+}
---
+++ my-omikuji-cli/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace MyOmikujiCli
+{
+    class Program
+    {
+
+        private static readonly string[] FORTUNES = {
+            "大吉",
+            "吉",
+            "中吉",
+            "小吉",
+            "末吉",
+            "凶",
+            "大凶",
+        };
+
+        private static readonly string[] LUCKY_ITEMS = {
+            "なし",
+            "ベルト",
+            "青ペン",
+            "iPhone",
+            "ビデオテープ",
+        };
+        static void Main(string[] args)
+        {
+            var fortune = FORTUNES.OrderBy((_) => { return Guid.NewGuid(); }).First();
+            var luckyItem = LUCKY_ITEMS.OrderBy((_) => { return Guid.NewGuid(); }).First();
+            
+            Console.WriteLine($"あなたの運勢は、{fortune}!ラッキーアイテムは{luckyItem}!");
+        }
+    }
+}
